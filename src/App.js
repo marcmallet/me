32a1f5bbc04eb697adff6deaa3c56e33300eea3e
Marc <marc@fundhouse.co.za>
2021-08-12 00:31:38 +0200

    Update splash screen with new logo and colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.png';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Coming Soon!
-        </p>        
+        <img src={logo} className="App-logo" width="300" alt="logo" />
+        <h1>Hi I'm, Marc Mallet</h1>
+        <span>Website Coming Soon!</span>        
       </header>
     </div>
   );
